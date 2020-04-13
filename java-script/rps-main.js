fee1c1219ebf9f2feebfdd778d2f645ff4b5c298
Harold Parra <gc.harold.parra@gmail.com>
2020-04-13 23:45:04 +0100

    created playRound function - returns winner - is case sensitive - uses prompt to get user choice

--- java-script/rps-main.js
+++ java-script/rps-main.js
@@ -13,12 +13,32 @@ function computerPlay() {
       computerSelection = "scissors";
       break;
   }
-
   return computerSelection;
 }
 
-console.log(computerPlay());
-console.log(computerPlay());
-console.log(computerPlay());
-console.log(computerPlay());
-console.log(computerPlay());
+function playRound(playerChoice, computerChoice) {
+  var resultOfRound = "";
+  if (playerChoice == "rock" && computerChoice == "rock") {
+    resultOfRound = "You tied.";
+  } else if (playerChoice == "rock" && computerChoice == "paper") {
+    resultOfRound = "You lost, paper beats rock.";
+  } else if (playerChoice == "rock" && computerChoice == "scissors") {
+    resultOfRound = "You won! Rock beats scissors!";
+  } else if (playerChoice == "paper" && computerChoice == "rock") {
+    resultOfRound = "You won! Paper beats rock!";
+  } else if (playerChoice == "paper" && computerChoice == "paper") {
+    resultOfRound = "You tied.";
+  } else if (playerChoice == "paper" && computerChoice == "scissors") {
+    resultOfRound = "You lost, scissors beat paper.";
+  } else if (playerChoice == "scissors" && computerChoice == "rock") {
+    resultOfRound = "You lost, rock beats scissors.";
+  } else if (playerChoice == "scissors" && computerChoice == "paper") {
+    resultOfRound = "You won! Scissors beat paper!";
+  } else if (playerChoice == "scissors" && computerChoice == "scissors") {
+    resultOfRound = "You tied.";
+  }
+  return resultOfRound;
+}
+
+var playerSelection = prompt("Choose: rock, paper or scissors").toLowerCase(),
+  computerSelection = computerPlay();
