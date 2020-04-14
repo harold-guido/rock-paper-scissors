2e45bbde783cc2edf2efb7e05cded196c1bcb2ac
Harold Parra <gc.harold.parra@gmail.com>
2020-04-14 02:06:39 +0100

    corrected file path syntax to link javascript link

--- java-script/rps-main.js
+++ java-script/rps-main.js
@@ -1,44 +1,92 @@
-function computerPlay() {
-  var randomInt = Math.floor(Math.random() * 3) + 1;
-  var computerSelection = "";
+function game() {
+  function computerPlay() {
+    var randomInt = Math.floor(Math.random() * 3) + 1;
+    var computerSelection = "";
 
-  switch (randomInt) {
-    case 1:
-      computerSelection = "rock";
-      break;
-    case 2:
-      computerSelection = "paper";
-      break;
-    case 3:
-      computerSelection = "scissors";
-      break;
+    switch (randomInt) {
+      case 1:
+        computerSelection = "rock";
+        break;
+      case 2:
+        computerSelection = "paper";
+        break;
+      case 3:
+        computerSelection = "scissors";
+        break;
+    }
+    return computerSelection;
   }
-  return computerSelection;
-}
-
-function playRound(playerChoice, computerChoice) {
-  var resultOfRound = "";
-  if (playerChoice == "rock" && computerChoice == "rock") {
-    resultOfRound = "You tied.";
-  } else if (playerChoice == "rock" && computerChoice == "paper") {
-    resultOfRound = "You lost, paper beats rock.";
-  } else if (playerChoice == "rock" && computerChoice == "scissors") {
-    resultOfRound = "You won! Rock beats scissors!";
-  } else if (playerChoice == "paper" && computerChoice == "rock") {
-    resultOfRound = "You won! Paper beats rock!";
-  } else if (playerChoice == "paper" && computerChoice == "paper") {
-    resultOfRound = "You tied.";
-  } else if (playerChoice == "paper" && computerChoice == "scissors") {
-    resultOfRound = "You lost, scissors beat paper.";
-  } else if (playerChoice == "scissors" && computerChoice == "rock") {
-    resultOfRound = "You lost, rock beats scissors.";
-  } else if (playerChoice == "scissors" && computerChoice == "paper") {
-    resultOfRound = "You won! Scissors beat paper!";
-  } else if (playerChoice == "scissors" && computerChoice == "scissors") {
-    resultOfRound = "You tied.";
+  function playRound(playerChoice, computerChoice) {
+    var resultOfRound = "";
+    if (playerChoice == "rock" && computerChoice == "rock") {
+      resultOfRound = "You tied.";
+    } else if (playerChoice == "rock" && computerChoice == "paper") {
+      resultOfRound = "You lost, paper beats rock.";
+      --counter;
+    } else if (playerChoice == "rock" && computerChoice == "scissors") {
+      resultOfRound = "You won! Rock beats scissors!";
+      ++counter;
+    } else if (playerChoice == "paper" && computerChoice == "rock") {
+      resultOfRound = "You won! Paper beats rock!";
+      ++counter;
+    } else if (playerChoice == "paper" && computerChoice == "paper") {
+      resultOfRound = "You tied.";
+    } else if (playerChoice == "paper" && computerChoice == "scissors") {
+      resultOfRound = "You lost, scissors beat paper.";
+      --counter;
+    } else if (playerChoice == "scissors" && computerChoice == "rock") {
+      resultOfRound = "You lost, rock beats scissors.";
+      --counter;
+    } else if (playerChoice == "scissors" && computerChoice == "paper") {
+      resultOfRound = "You won! Scissors beat paper!";
+      ++counter;
+    } else if (playerChoice == "scissors" && computerChoice == "scissors") {
+      resultOfRound = "You tied.";
+    }
+    return resultOfRound;
   }
-  return resultOfRound;
-}
 
-var playerSelection = prompt("Choose: rock, paper or scissors").toLowerCase(),
+  var playerSelection = "",
+    computerSelection = "",
+    counter = 0,
+    winner = "";
+
+  alert("Welcome to a 5 round game of rock paper scissors!!");
+
+  playerSelection = prompt("Choose: rock, paper or scissors").toLowerCase();
   computerSelection = computerPlay();
+  alert("The computer has chosen " + computerSelection);
+  alert(playRound(playerSelection, computerSelection));
+
+  playerSelection = prompt("Choose: rock, paper or scissors").toLowerCase();
+  computerSelection = computerPlay();
+  alert("The computer has chosen " + computerSelection);
+  alert(playRound(playerSelection, computerSelection));
+
+  playerSelection = prompt("Choose: rock, paper or scissors").toLowerCase();
+  computerSelection = computerPlay();
+  alert("The computer has chosen " + computerSelection);
+  alert(playRound(playerSelection, computerSelection));
+
+  playerSelection = prompt("Choose: rock, paper or scissors").toLowerCase();
+  computerSelection = computerPlay();
+  alert("The computer has chosen " + computerSelection);
+  alert(playRound(playerSelection, computerSelection));
+
+  playerSelection = prompt("Choose: rock, paper or scissors").toLowerCase();
+  computerSelection = computerPlay();
+  alert("The computer has chosen " + computerSelection);
+  alert(playRound(playerSelection, computerSelection));
+
+  alert(
+    `${
+      counter > 0
+        ? "The game is finished!! Congratulations, you won!!"
+        : counter < 0
+        ? "The game is finished. You have lost :("
+        : "The game is finished. You tied!"
+    }`
+  );
+}
+
+game();
